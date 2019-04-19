b5f7b46a3a7547594c6ade1ea7dc17c0eb6c2178
Gary Mannion <gkmannion@hotmail.com>
2019-04-19 13:24:17 +0100

    search node class created

---
+++ AI-Project/src/ie/gmit/sw/ai/searchNode.java
@@ -0,0 +1,20 @@
+package ie.gmit.sw.ai;
+
+class searchNode {
+    int x;
+    int y;
+    
+    searchNode(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Search
+{  
+	
+    public searchNode findPath(char[][] matrix, int row, int col) {
+        return null;
+    }
+    
+}
