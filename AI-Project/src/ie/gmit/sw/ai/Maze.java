1f0e0a1d913e4f06517a786e0120fc3b831c821d
Gary Mannion <gkmannion@hotmail.com>
2019-04-20 20:59:13 +0100

    created thread for enemy spiders

--- AI-Project/src/ie/gmit/sw/ai/Maze.java
+++ AI-Project/src/ie/gmit/sw/ai/Maze.java
@@ -1,7 +1,8 @@
 package ie.gmit.sw.ai;
 
 public class Maze {
-	private char[][] maze; // An array does not lend itself to the type of mazge generation alogs we use in
+
+	private static char[][] maze; // An array does not lend itself to the type of mazge generation alogs we use in
 							// the labs. There are no "walls" to carve...
 
 	public Maze(int dimension) {
@@ -48,11 +49,12 @@ public class Maze {
 			if (maze[row][col] == replace) {
 				maze[row][col] = feature;
 				// need to create thread class for spiders
-				/*
-				 * int[] pos = {row, col};
-				 * 
-				 * SpiderThread st = new SpiderThread(pos, feature); st.start();
-				 */
+				
+				  int[] pos = {row, col};
+				  
+				  SpiderThread st = new SpiderThread(pos, feature, 100); 
+				  st.start();
+				 
 
 				counter++;
 			}
@@ -80,20 +82,20 @@ public class Maze {
 		return row > 1 && maze[row - 1][col] == '\u0020' && maze[row - 1][col + 1] == '\u0020';
 	}
 
-	public char[][] getMaze() {
-		return this.maze;
+	public static char[][] getMaze() {
+		return maze;
 	}
 
 	public char get(int row, int col) {
-		return this.maze[row][col];
+		return Maze.maze[row][col];
 	}
 
 	public void set(int row, int col, char c) {
-		this.maze[row][col] = c;
+		Maze.maze[row][col] = c;
 	}
 
 	public int size() {
-		return this.maze.length;
+		return Maze.maze.length;
 	}
 
 	public String toString() {
--- AI-Project/src/ie/gmit/sw/ai/SpiderThread.java
+++ AI-Project/src/ie/gmit/sw/ai/SpiderThread.java
@@ -1,5 +1,183 @@
 package ie.gmit.sw.ai;
 
-public class SpiderThread {
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JOptionPane;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+
+public class SpiderThread extends Thread{
+
+	ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+	
+	//keep enemy pos
+	private int[] pos = new int[2];
+	private char val;
+	
+	private int venom = 10;
+	private int strength = 0;
+	
+	SpiderThread(int[] p, char v, int s){
+		
+		// values for conditions
+		this.pos = p;
+		this.val= v;
+		this.strength = s;
+		
+	}
+	
+	protected void attack() {
+		
+		double attack = getAttackValue();
+		
+		double potency = getPotencyValue();
+		
+		int damage = getDamageValue(attack, potency);
+	
+		try {
+		
+			ControlledSprite.getInstance().setHealth(ControlledSprite.getInstance().getHealth() - damage);
+			
+			JOptionPane.showMessageDialog(null, getSpidertype() +" Spider Dealt " + damage + " damage"
+					+ "\nYou have " +ControlledSprite.getInstance().getHealth() +" Health left");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public String getSpidertype() {
+		
+		switch(val) {
+		case '6':
+			return "Black";
+		case '7':
+			return "Blue";
+		case '8':
+			return "Brown";
+		case '9':
+			return "Green";
+		case ':':
+			return "Grey";
+		case ';':
+			return "Orange";
+		case '<':
+			return "Red";
+		case '=':
+			return "Yellow";
+		default:
+			return "";
+		}
+	}
+	
+	public double getAttackValue() {
+		
+		FIS file = FIS.load("resources/fuzzy/damageLogic.fcl", true);
+		
+		FunctionBlock damageLogic = file.getFunctionBlock("AttackDamage");
+		
+		damageLogic.setVariable("attack", strength);
+		
+		damageLogic.evaluate();
+		
+		return damageLogic.getVariable("damage").getValue();
+		
+	}
+	
+	public double getPotencyValue() {
+		
+		FIS file = FIS.load("resources/fuzzy/venom.fcl", true);
+		
+		FunctionBlock damageLogic = file.getFunctionBlock("VenomPotency");
+		
+		damageLogic.setVariable("venom", venom);
+		
+		damageLogic.evaluate();
+		
+		return damageLogic.getVariable("potency").getValue();
+		
+	}
+	
+	public int getDamageValue(double attack, double potency) {
+		
+		FIS file = FIS.load("resources/fuzzy/damage.fcl", true);
+		
+		FunctionBlock damageLogic = file.getFunctionBlock("damageoutput");
+		
+		damageLogic.setVariable("output", attack);
+		
+		damageLogic.setVariable("potencyofvenom", potency);
+		
+		damageLogic.evaluate();
+		
+		return (int)damageLogic.getVariable("damagedealt").getValue();
+		
+	}
+
+	public void findPlayer() throws Exception {
+		
+		GameView.getInstance();
+		GameView.getMaze();
+		
+		char[][] matrix = Maze.getMaze();
+		
+		searchNode node = null;
+
+		move(pos[0], pos[1], node, val);
+		
+	}
+        
+	public void move(int row, int col, searchNode node, char val) throws Exception {
+		
+		GameView.getInstance();
+		
+		int newRow = node.x;
+		int newCol = node.y;
+		
+		GameView.setMaze(pos[0], pos[1], '\u0020');
+		GameView.setMaze(newRow, newCol, val);
+
+		pos[0] = newRow;
+		pos[1]= newCol;
+		
+	}
+	
+	public int[] getPos() {
+		return pos;
+	}
+	
+	public void setPos(int[] pos) {
+		this.pos = pos;
+	}
+
+	public char getVal() {
+		return val;
+	}
+
+
+	public void setVal(char val) {
+		this.val = val;
+	}
+
+	public int getVenom() {
+		return venom;
+	}
+
+	public void setVenom(int venom) {
+		this.venom = venom;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
 
 }
